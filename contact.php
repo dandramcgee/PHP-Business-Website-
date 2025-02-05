e47e9181aa100e2c7faa11988b29dfaa14e37d35
“dandramcgee” <dandramcgee525@gmail.com>
2025-02-05 08:51:15 +0500

    commit

--- contact.php
+++ contact.php
@@ -23,7 +23,7 @@ $query = new Database();
   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
   <link href="assets/vendor/aos/aos.css" rel="stylesheet">
   <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightboFx/css/glightbox.min.css" rel="stylesheet">
   <link href="assets/css/main.css" rel="stylesheet">
 </head>
 
