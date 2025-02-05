659f897c28acebcce95115dcfe2b760d5fb89f90
“dandramcgee” <dandramcgee525@gmail.com>
2025-02-05 10:23:06 +0500

    commit

--- admin/index.php
+++ admin/index.php
@@ -15,103 +15,103 @@
 </head>
 
 <body class="hold-transition sidebar-mini">
+
     <div class="wrapper">
+        <?php include 'includes/header.php' ?>
+        <div class="content-wrapper">
 
-        <!-- Navbar -->
-        <?php include 'includes/navbar.php' ?>
-
-        <?php active('Dashboard', 1) ?>
-        <div class="main-header">
-            <!-- Page path -->
             <?php
             $arr = array(
                 ["title" => "Home", "url" => "./"],
-                ["title" => "Admin", "url" => "#"],
+                ["title" => "Dashboard", "url" => "#"],
             );
-            pagePath('Dashboard', $arr);
+            renderHeader('Dashboard', $arr);
             ?>
-        </div>
 
-        <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
+            <!-- Content Wrapper. Contains page content -->
+            <div class="content-wrapper">
 
-            <br>
+                <br>
 
-            <section class="content">
-                <div class="container-fluid">
+                <section class="content">
+                    <div class="container-fluid">
 
-                    <div class="row">
-                        <div class="col-lg-3 col-6">
+                        <div class="row">
+                            <div class="col-lg-3 col-6">
 
-                            <div class="small-box bg-info">
-                                <div class="inner">
-                                    <h3>150</h3>
+                                <div class="small-box bg-info">
+                                    <div class="inner">
+                                        <h3>150</h3>
 
-                                    <p>New Orders</p>
+                                        <p>New Orders</p>
+                                    </div>
+                                    <div class="icon">
+                                        <i class="ion ion-bag"></i>
+                                    </div>
+                                    <a href="#" class="small-box-footer">More info <i
+                                            class="fas fa-arrow-circle-right"></i></a>
                                 </div>
-                                <div class="icon">
-                                    <i class="ion ion-bag"></i>
+                            </div>
+
+                            <div class="col-lg-3 col-6">
+
+                                <div class="small-box bg-success">
+                                    <div class="inner">
+                                        <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+                                        <p>Bounce Rate</p>
+                                    </div>
+                                    <div class="icon">
+                                        <i class="ion ion-stats-bars"></i>
+                                    </div>
+                                    <a href="#" class="small-box-footer">More info <i
+                                            class="fas fa-arrow-circle-right"></i></a>
                                 </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                            </div>
+
+                            <div class="col-lg-3 col-6">
+                                <div class="small-box bg-warning">
+                                    <div class="inner">
+                                        <h3>44</h3>
+
+                                        <p>User Registrations</p>
+                                    </div>
+                                    <div class="icon">
+                                        <i class="ion ion-person-add"></i>
+                                    </div>
+                                    <a href="#" class="small-box-footer">More info <i
+                                            class="fas fa-arrow-circle-right"></i></a>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-3 col-6">
+
+                                <div class="small-box bg-danger">
+                                    <div class="inner">
+                                        <h3>65</h3>
+
+                                        <p>Unique Visitors</p>
+                                    </div>
+                                    <div class="icon">
+                                        <i class="ion ion-pie-graph"></i>
+                                    </div>
+                                    <a href="#" class="small-box-footer">More info <i
+                                            class="fas fa-arrow-circle-right"></i></a>
+                                </div>
+
                             </div>
                         </div>
 
-                        <div class="col-lg-3 col-6">
-
-                            <div class="small-box bg-success">
-                                <div class="inner">
-                                    <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-                                    <p>Bounce Rate</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-stats-bars"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-3 col-6">
-                            <div class="small-box bg-warning">
-                                <div class="inner">
-                                    <h3>44</h3>
-
-                                    <p>User Registrations</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-person-add"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-3 col-6">
-
-                            <div class="small-box bg-danger">
-                                <div class="inner">
-                                    <h3>65</h3>
-
-                                    <p>Unique Visitors</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-pie-graph"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-
-                        </div>
                     </div>
+                </section>
+            </div>
 
-                </div>
-            </section>
+            <!-- Main Footer -->
+            <?php include 'includes/footer.php'; ?>
         </div>
 
-        <!-- Main Footer -->
-        <?php include 'includes/footer.php'; ?>
-    </div>
-
-    <!-- SCRIPTS -->
-    <?php include 'includes/js.php' ?>
+        <!-- SCRIPTS -->
+        <?php include 'includes/js.php' ?>
 </body>
 
 </html>
