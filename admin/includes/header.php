e9e5cc873b1e0ade27ba110a368b9cb82384ec9e
“dandramcgee” <dandramcgee525@gmail.com>
2025-02-05 12:23:53 +0500

    commit

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -51,54 +51,6 @@ $menuItems = [
         ],
     ]
 ];
-?>
-
-<!-- Count the number of unchecked messages -->
-<?php $count = (new Database())->eQuery("SELECT COUNT(*) AS no_checked_count FROM messages WHERE status = 'no_checked';")[0]['no_checked_count']; ?>
-
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-<nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href="./" class="nav-link">Home</a>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a onclick="logout()" class="nav-link">Logout</a>
-        </li>
-    </ul>
-
-    <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">
-        <!-- Messages Dropdown Menu -->
-        <li class="nav-item dropdown">
-            <a class="nav-link" href="messages.php">
-                <i class="far fa-comments"></i>
-                <span class="badge badge-danger navbar-badge" <?php echo $count === 0 ? 'style="display: none;"' : ''; ?>>
-                    <?php echo $count; ?>
-                </span>
-            </a>
-        </li>
-    </ul>
-</nav>
-
-<?php
-$currentPage = basename($_SERVER['SCRIPT_NAME']);
-$pageTitle = "";
-
-$menuItems = [
-    [
-        "menuTitle" => "Menu",
-        "icon" => "fas fa-home",
-        "pages" => [
-            ["title" => "Home", "url" => "index.php"],
-            ["title" => "Alerts", "url" => "alerts.php"],
-        ],
-    ],
-];
 
 $breadcrumbItems = [];
 foreach ($menuItems as $menuItem) {
@@ -131,6 +83,38 @@ foreach ($menuItems as $menuItem) {
         </div>
     </div>
 </div>
+
+<!-- Count the number of unchecked messages -->
+<?php $count = (new Database())->eQuery("SELECT COUNT(*) AS no_checked_count FROM messages WHERE status = 'no_checked';")[0]['no_checked_count']; ?>
+
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a href="./" class="nav-link">Home</a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a onclick="logout()" class="nav-link">Logout</a>
+        </li>
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+        <!-- Messages Dropdown Menu -->
+        <li class="nav-item dropdown">
+            <a class="nav-link" href="messages.php">
+                <i class="far fa-comments"></i>
+                <span class="badge badge-danger navbar-badge" <?php echo $count === 0 ? 'style="display: none;"' : ''; ?>>
+                    <?php echo $count; ?>
+                </span>
+            </a>
+        </li>
+    </ul>
+</nav>
 
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="./" class="brand-link">
