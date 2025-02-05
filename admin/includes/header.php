988e9e5f4ce0faca74efaa85c30c651d45635bc2
“dandramcgee” <dandramcgee525@gmail.com>
2025-02-05 11:11:25 +0500

    commit

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -53,43 +53,34 @@ $menuItems = [
 ];
 ?>
 
+<?php
+// Count the number of unchecked messages
+$count = (new Database())->eQuery("SELECT COUNT(*) AS no_checked_count FROM messages WHERE status = 'no_checked';")[0]['no_checked_count'];
+?>
+
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left side of the navbar -->
+    <!-- Left navbar links -->
     <ul class="navbar-nav">
         <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" role="button"><i class="fas fa-bars"></i></a>
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
             <a href="./" class="nav-link">Home</a>
         </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a onclick="logout()" class="nav-link">Logout</a>
+        </li>
     </ul>
 
-    <!-- Search form -->
-    <form class="form-inline ml-3">
-        <div class="input-group input-group-sm">
-            <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search"
-                name="search">
-            <div class="input-group-append">
-                <button class="btn btn-navbar" type="submit">
-                    <i class="fas fa-search"></i>
-                </button>
-            </div>
-        </div>
-    </form>
-
-    <!-- Right side of the navbar -->
+    <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
+        <!-- Messages Dropdown Menu -->
         <li class="nav-item dropdown">
-            <a class="nav-link" href="#messages">
+            <a class="nav-link" href="messages.php">
                 <i class="far fa-comments"></i>
-                <span class="badge badge-danger navbar-badge">2</span>
-            </a>
-        </li>
-        <!-- Notifications dropdown -->
-        <li class="nav-item dropdown">
-            <a class="nav-link" href="#notifications">
-                <i class="far fa-bell"></i>
-                <span class="badge badge-warning navbar-badge">5</span>
+                <span class="badge badge-danger navbar-badge" <?php echo $count === 0 ? 'style="display: none;"' : ''; ?>>
+                    <?php echo $count; ?>
+                </span>
             </a>
         </li>
     </ul>
@@ -191,3 +182,21 @@ function renderHeader($pageTitle, $breadcrumbItems)
     <?php
 }
 ?>
+
+<script>
+    function logout() {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You will be logged out!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, log me out!'
+        }).then((result) => {
+            if (result.value) {
+                window.location.href = './logout/';
+            }
+        });
+    }
+</script>
