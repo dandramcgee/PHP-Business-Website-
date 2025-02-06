3b7467e330dbeb2be2fcfb0130ce7c12be9ed3cb
“dandramcgee” <dandramcgee525@gmail.com>
2025-02-06 11:22:18 +0500

    commit

--- admin/login/index.php
+++ admin/login/index.php
@@ -104,8 +104,9 @@ if (isset($_POST['submit'])) {
 
         <form method="post" action="">
             <div class="form-group">
-                <label for="username">Username or Email</label>
-                <input type="text" id="username" name="username" required maxlength="255">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" required maxlength="30">
+                <small id="username-error" style="color: red;"></small>
             </div>
             <div class="form-group">
                 <label for="password">Password</label>
@@ -116,31 +117,51 @@ if (isset($_POST['submit'])) {
                 </div>
             </div>
             <div class="form-group">
-                <button type="submit" name="submit" id="submit">Login</button>
+                <button type="submit" name="submit" id="submit" disabled>Login</button>
             </div>
         </form>
 
+        <div class="text-center">
+            <p>Don't have an account? <a href="../signup/">Sign Up</a></p>
+        </div>
+
     </div>
 
+    <script src="../src/js/sweetalert2.js"></script>
     <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            document.getElementById('toggle-password').addEventListener('click', function () {
-                const passwordField = document.getElementById('password');
-                const toggleIcon = this.querySelector('i');
+        const usernameField = document.getElementById('username');
+        const usernameError = document.getElementById('username-error');
+        const submitButton = document.getElementById('submit');
 
-                if (passwordField.type === 'password') {
-                    passwordField.type = 'text';
-                    toggleIcon.classList.remove('fa-eye');
-                    toggleIcon.classList.add('fa-eye-slash');
-                } else {
-                    passwordField.type = 'password';
-                    toggleIcon.classList.remove('fa-eye-slash');
-                    toggleIcon.classList.add('fa-eye');
-                }
-            });
+        function validateForm() {
+            const username = usernameField.value;
+            const usernamePattern = /^[a-zA-Z0-9_]+$/;
+            if (!usernamePattern.test(username)) {
+                usernameError.textContent = "Username can only contain letters, numbers, and underscores!";
+                submitButton.disabled = true;
+            } else {
+                usernameError.textContent = "";
+                submitButton.disabled = false;
+            }
+        }
+
+        usernameField.addEventListener('input', validateForm);
+
+        document.getElementById('toggle-password').addEventListener('click', function () {
+            const passwordField = document.getElementById('password');
+            const toggleIcon = this.querySelector('i');
+
+            if (passwordField.type === 'password') {
+                passwordField.type = 'text';
+                toggleIcon.classList.remove('fa-eye');
+                toggleIcon.classList.add('fa-eye-slash');
+            } else {
+                passwordField.type = 'password';
+                toggleIcon.classList.remove('fa-eye-slash');
+                toggleIcon.classList.add('fa-eye');
+            }
         });
     </script>
 
-</body>
 
 </html>
